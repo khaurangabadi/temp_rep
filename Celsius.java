7d94415a8239a6b12a71178da4357ca532ce67db
khaurangabadi <106445220+khaurangabadi@users.noreply.github.com>
2022-06-23 19:54:44 +0530

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,10 @@
+class Celsius
+{
+   public static void main(String ar[])
+   {
+             float cel=34.5f;
+             float fehrenheight=((cel*9)/5)+32;
+            System.out.println(" Temperature in fehrengeit is"+fehrenheight);
+ }
+}
+      
